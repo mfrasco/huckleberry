a8d07ffdda80fcd580d88de9a440b143024a9b35
mfrasco <mfrasco6@gmail.com>
2024-08-29 18:14:39 -0700

    sql bugs

--- huckleberry/schema.sql
+++ huckleberry/schema.sql
@@ -1,7 +1,7 @@
 drop table if exists secret_objects;
 create table secret_objects (
     id integer primary key autoincrement,
-    secret_object text not null,
+    secret_object text not null
 );
 
 drop table if exists clues;
@@ -10,5 +10,5 @@ create table clues (
     secret_object_id integer not null,
     clue_object text not null,
     attribute integer not null,
-    order integer not null,
+    clue_order integer not null
 );
